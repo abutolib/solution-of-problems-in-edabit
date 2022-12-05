b47b5e026acf8442c7c0139e1ffde2dc7867c8ae
abutolib <abutolib12042002@gmail.com>
2022-12-05 21:09:06 +0500

    Innitial commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// 1-MASALA
+
+// function addition(a, b) {
+// 	return a + b;
+// }
+
+// 2-MASALA
+
+// function convert(minutes) {
+// 	return minutes * 60;
+// }
+
+// 3-MASALA
+
+// function addition(num) {
+// 	return ++num;
+// }
+
+// 4-MASALA
+
+// function triArea(base, height) {
+// 	return base * height / 2;
+// }
+
+// 5-MASALA
+
+// function calcAge(age) {
+// 	return age * 365;
+// }
+
+// 6-MASALA  
+
+// function remainder(x, y) {
+// 	return x % y; 
+// }
+
+// 7-MASALA
+
+// function footballPoints(wins, draws, losses) {
+// 	return wins * 3 + draws * 1 ;
+// }
+
+// 8-MASALA
+
+// function points(twoPointers, threePointers) {
+// 	return twoPointers*2 + threePointers*3
+// }
+
+// 9-MASALA
+
+// function lessThan100(a, b) {
+// 	return a + b < 100 ? true :false;
+// }
+
+// 10-MASALA
+
+// function animals(chickens, cows, pigs) {
+// 	return chickens * 2 + cows * 4 + pigs * 4
+// }
